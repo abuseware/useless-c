a7db9cd87e315b5665172eb6916ac70422b9f094
Licho <abuseware@users.noreply.github.com>
2014-07-15 15:32:00 +0200

    Bytecode loader - replaced file operations with mmap and added assertions

--- loader.c
+++ loader.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
-{
-	char* bytecode;
-	FILE* pf;
-	int fsiz;
-	
-	puts("Bytecode loader by thc_flow");
-	
-	pf = fopen(argv[1],"rb");
-	if(pf != NULL){
-		fseek(pf,0,SEEK_END);
-		fsiz=ftell(pf);
-		rewind(pf);
-		bytecode = (char*) malloc(sizeof(char)*fsiz);
-		if(bytecode != NULL){
-			fread(bytecode,1,fsiz,pf);
-			(*(void(*)(void)) bytecode)();
-		} else return 1;
-		free(bytecode);
-	} else return 1;
-	fclose(pf);
-	
-	return 0;
+int main(int argc, char **argv) {
+  int fd;
+  struct stat fs;
+  void *payload;
+
+  fd = open(argv[1], O_RDONLY);
+  assert(fd != -1);
+
+  assert(fstat(fd, &fs) != -1);
+
+  payload = mmap(0, fs.st_size, PROT_READ | PROT_EXEC, MAP_SHARED, fd, 0);
+  assert(payload != MAP_FAILED);
+
+  (*(void (*)(void))payload)();
+
+  assert(munmap(payload, fs.st_size) != -1);
+  assert(close(fd) != -1);
+
+  return EXIT_SUCCESS;
 }
-
